fc81d6dcee936dd270f3d1354b7a872886e2ce31
GianluGiarde <gianlugiarde@gmail.com>
2023-11-07 10:12:33 +0100

    fix Ticket class

--- src/main/java/org/lessons/Ticket.java
+++ src/main/java/org/lessons/Ticket.java
@@ -5,7 +5,6 @@ public class Ticket {
     private int kilometers;
     private int age;
     private double basePrice;
-    private double finalPrice;
 
     public Ticket(int kilometers, int age) throws IllegalArgumentException {
         checkPositive(kilometers);
@@ -15,24 +14,6 @@ public class Ticket {
         this.age = age;
 
         this.basePrice = kilometers * 0.21;
-    }
-
-    public int getKilometers() {
-        return kilometers;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setKilometers(int kilometers) throws IllegalArgumentException {
-        checkPositive(kilometers);
-        this.kilometers = kilometers;
-    }
-
-    public void setAge(int age) throws IllegalArgumentException {
-        checkPositive(age);
-        this.age = age;
     }
 
     private void checkPositive(int num) throws IllegalArgumentException {
